caccccd945d4543c5308e9dc3e00dde36a3318f6
Evgeniy Viktorov <yark1y@i.ua>
2018-10-23 21:17:56 +0300

    initial commit

---
+++ src/main/java/ua/help/agro/core/AgroHelpCoreApplication.java
@@ -0,0 +1,12 @@
+package ua.help.agro.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroHelpCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroHelpCoreApplication.class, args);
+	}
+}
