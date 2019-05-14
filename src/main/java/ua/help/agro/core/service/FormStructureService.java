27d1ae683851401dc6ed4775b099bdae1328c31a
Evgeniy VIktorov <evgeniy.viktorov@quantag-it.com>
2019-05-14 22:46:26 +0300

    cheats on

--- src/main/java/ua/help/agro/core/service/FormStructureService.java
+++ src/main/java/ua/help/agro/core/service/FormStructureService.java
@@ -1,21 +1,46 @@
 package ua.help.agro.core.service;
 
 import org.springframework.stereotype.Service;
+import ua.help.agro.core.domain.Field;
+import ua.help.agro.core.domain.Form;
 import ua.help.agro.core.domain.FormStructure;
+import ua.help.agro.core.domain.User;
 import ua.help.agro.core.repository.FormStructureRepository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Service
 public class FormStructureService {
     private FormStructureRepository formStructureRepository;
+    private UserService userService;
+    private FieldService fieldService;
 
-    public FormStructureService(FormStructureRepository formStructureRepository) {
+
+    public FormStructureService(FormStructureRepository formStructureRepository, UserService userService, FieldService fieldService) {
         this.formStructureRepository = formStructureRepository;
+        this.userService = userService;
+        this.fieldService = fieldService;
     }
 
-    public void save(FormStructure formStructure) {
-        formStructureRepository.save(formStructure);
+    public void save(List<FormStructure> formStructure) {
+        formStructure.forEach(e -> formStructureRepository.save(e));
+        User evgeniyViktorov = userService.getUserById(2L);
+        Form newForm = Form
+                .builder()
+                .formName("ФПрисланая админом форма от " + LocalDate.now().toString())
+                .formStructures(formStructure)
+                .dateUpdated(LocalDate.now())
+                .dueDate(LocalDate.now().plusDays(7))
+                .isAccepted(true)
+                .isCompleted(false)
+                .submittedBy(evgeniyViktorov)
+                .build();
+        Field weedField = fieldService.getFieldById(8L);
+        List<Form> forms = weedField.getForms();
+        forms.add(newForm);
+        weedField.setForms(forms);
+        fieldService.save(weedField);
     }
 
     public FormStructure getFormFieldStructureById(Long id) {
